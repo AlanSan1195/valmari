5b39bd961608909006fd502d4c1b91ea8be2f3e4
Alan San <alansandovalt1@gmail.com>
2025-10-01 21:51:54 -0600

    chore: add path alias and update asset references in layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
