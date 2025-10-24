d279fb36684bca919d9a0d9481d9a227ccf8011b
Alan San <alansandovalt1@gmail.com>
2025-10-23 19:28:26 -0600

    separar constantes y info de packs para refactorizar codigo

---
+++ src/services/constantes.js
@@ -0,0 +1,10 @@
+export const navLinks = [
+  { name: "Inicio", href: "#home" },
+  { name: "Packs", href: "#packs" },
+  { name: "Nosotros", href: "#nosotros" },
+  { name: "Galeria", href: "#galeria" },
+  { name: "Clientes", href: "#clientes" },
+];
+
+export const whatsappNumber = "5512345678"; // Reemplaza con tu número
+export const whatsappMessage = "Hola, me gustaría información sobre sus servicios";
